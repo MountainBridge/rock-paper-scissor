3f0859e00175a2d5437f3eb82730da39e95fd379
Nidhi Verma <nidhi199.verma@gmail.com>
2019-01-16 10:40:24 +0530

    commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import {MatToolbarModule, MatCardModule } from '@angular/material';
+
+@NgModule({
+    imports:[MatToolbarModule, MatCardModule],
+    exports:[MatToolbarModule, MatCardModule]
+})
+export class MaterialModule{
+    
+}
